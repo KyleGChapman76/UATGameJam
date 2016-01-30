989e35f43142987b0ccf7b2054221657cd15badd
Griffin <gtullgau@asu.edu>
2016-01-30 14:54:28 -0700

    Demon Circle

---
+++ DemonRitual/Assets/Scripts/MagicCircle.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class MagicCircle : MonoBehaviour
+{
+	public float radius;
+	public GameObject testObject;
+
+	private int numOfItems;
+	public List <GameObject> items;
+
+	// Use this for initialization
+	void Start () 
+	{
+		numOfItems = 0;
+		items = new List<GameObject>();
+	}
+
+	void AddToCircle(GameObject newItem) // Adds a component to the component list
+	{	
+		numOfItems++;
+		items.Add(newItem);
+		newItem.transform.parent = gameObject.transform;
+		RepositionItems();
+
+
+	}
+
+	private void RepositionItems () // Puts the items in the circle
+	{
+		float angle = 2*(Mathf.PI)/numOfItems;
+		float posAngle = Mathf.PI/2;
+		for(int i = 0; i < numOfItems; i++)
+		{
+			float posX = Mathf.Cos (posAngle)*radius;
+			float posY = Mathf.Sin (posAngle)*radius;
+
+			items[i].transform.localPosition = new Vector3 (posX, posY, 0);
+
+			posAngle += angle; 
+
+		}
+	}
+
+	void Update() {
+		if (Input.GetKeyDown ("space"))
+		{
+			GameObject circleItem = Instantiate(testObject, new Vector3(0,0,0), Quaternion.identity) as GameObject;
+			AddToCircle (circleItem);
+		}
+
+	}
+}
