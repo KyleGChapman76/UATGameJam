88af5a7b6583205bb77b8f332efe355c8316e2ba
Kyle Chapman <xeuoruxgmail.com>
2016-01-30 09:39:10 -0700

    Basic main menu

---
+++ DemonRitual/Assets/MainMenu.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour
+{
+    public string level1Name;
+
+    public void BeginGame ()
+    {
+        Application.LoadLevel(level1Name);
+    }
+
+    public void ExitGame ()
+    {
+        Application.Quit();
+    }
+}
