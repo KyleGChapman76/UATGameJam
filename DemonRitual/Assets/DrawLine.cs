a32f8c8e699bf624e344104eae1e39c2cac101fe
Kyle Chapman <xeuoruxgmail.com>
2016-01-30 15:16:24 -0700

    Line drawing research and ghost enemy

---
+++ DemonRitual/Assets/DrawLine.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DrawLine : MonoBehaviour {
+
+	public GameObject player1;
+	public GameObject player2;
+	public LineRenderer renderer;
+
+	void Update () {
+		renderer.SetWidth(0.2F, 0.2F);
+		renderer.SetPosition(0, player1.transform.position);
+		renderer.SetPosition(1, player2.transform.position);
+	}
+}
